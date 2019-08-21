23dd51bd7edf1c549f20204b935ae625981bdad9
rafael <rafaelmarques76076@gmail.com>
2019-08-21 20:58:42 +0100

    Basic Game functionality is up and working. The game can now be played from begging to start, one round at a time, until the money runs out. @TODO refactor, refactor, refactor. but first, basic styling

---
+++ src/stateHandle/selectors.tsx
@@ -0,0 +1,22 @@
+export const getDealerCards = (state) => { 
+  return state.game.dealer.cards.cards
+} 
+
+export const getPlayersBet = (state) => { 
+  return state.game.players[0].bet
+} 
+
+export function getPlayersCards(state, indexPlayer=0) {
+  if (state.game.players && state.game.players[indexPlayer]) {
+    return state.game.players[indexPlayer].cards.cards
+  }
+  return [];
+}
+
+export function getPlayersCreditAmmount(state, indexPlayer=0) {
+  return state.game.players[indexPlayer].credit;
+}
+
+export function getPlayersUsername(state, indexPlayer=0) {
+  return state.game.players[indexPlayer].username;
+}
