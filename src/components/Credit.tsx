56b3f0eab218d885ab32aeacf304c3bcaeacae5b
rafael <rafaelmarques76076@gmail.com>
2019-08-31 15:51:01 +0100

    Create basic animation for gold coins

--- src/components/Credit.tsx
+++ src/components/Credit.tsx
@@ -1,17 +1,54 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
+import { animated, useSpring } from 'react-spring';
 
-const CreditPlayer = ({ ammount }) => (
-  <View style={styles.container}>
-    <Image 
-      style={styles.image}
-      source={require('../../assets/gold-coins.png')} />
-    <Text 
-      style={styles.text}>
-      {ammount}
-    </Text>
-  </View>
-);
+const AnimatedImage = animated(Image)
+
+const CreditPlayer = ({ ammount }) => {
+  // set state to track credit change
+  const [credit, setCredit] = useState(0);
+
+  // this is called on prop or state update
+  useEffect(() => {
+    if (credit !== ammount) {
+      setTimeout(setCredit, 1500, ammount)
+    }
+  })
+
+  // declare possible animations
+  const springLarger = useSpring({
+    from: { width: 20, height: 20 },
+    to: async (next) => {
+      await next({ width: 27, height: 27})
+      await next({ width: 20, height: 20},)
+    }
+  })
+
+  const springSmaller = useSpring({
+    from: { width: 20, height: 20 },
+    to: async (next) => {
+      await next({ width: 15, height: 15})
+      await next({ width: 20, height: 20},)
+    }
+  })
+
+  // select animation
+  let spring = {}
+  if (ammount > credit) { spring = springLarger }
+  if (ammount < credit) { spring = springSmaller }
+
+  return (
+    <View style={styles.container}>
+      <AnimatedImage
+        style={{...styles.image, ...spring}}
+        source={require('../../assets/gold-coins.png')} />
+      <Text 
+        style={styles.text}>
+        {ammount}
+      </Text>
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
